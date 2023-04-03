dad31478910ae6a9e85a8f9fa0dbb308832ed51b
GregoryPostDavis <gregoryp500@gmail.com>
2023-04-03 18:30:06 -0500

    Added TCP Connection (Server and Client Side) Updated spacing on comments to align with PyCharm's practices

--- proj_client.py
+++ proj_client.py
@@ -2,18 +2,30 @@ import socket
 
 IP = "127.0.0.1"
 PORT = 1234
-client_id = 'mockclient' # hardcoded client ID: has to be in subscribed users list
+client_id = 'mockclient'  # hardcoded client ID: has to be in subscribed users list
 
 # create socket
 server_address = ("127.0.0.1", 1234)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # AF_INET = internet, SOCK_DGRAM = UDP
-print("Socket successfully created") # debug
+print("Socket successfully created")  # debug
 
 # HELLO
-hello_message = f'HELLO({client_id})' # protocol message: syntax may be changed
+hello_message = f'HELLO({client_id})'  # protocol message: syntax may be changed
 client_socket.sendto(hello_message.encode(), server_address)
 response, server_address = client_socket.recvfrom(1024) # Wait for the server to respond
-print("Server says: " + response.decode()) # Debug
+print("Server says: " + response.decode())  # Debug
 
 # close the socket
 client_socket.close()
+
+#create tcp socket
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5678
+tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_client.connect((TCP_IP, TCP_PORT))
+
+tcp_client.send("Hello Server!".encode('utf-8'))
+print(tcp_client.recv(1024).decode('utf-8'))
+tcp_client.send("Bye!".encode('utf-8'))
+print(tcp_client.recv(1024).decode('utf-8'))
+tcp_client.close()
--- proj_server.py
+++ proj_server.py
@@ -4,11 +4,11 @@ IP = "127.0.0.1"
 PORT = 1234
 
 # Create socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # AF_INET: internet, SOCK_DGRAM: UDP
-print ("Socket successfully created") # debug
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # AF_INET: internet, SOCK_DGRAM: UDP
+print("Socket successfully created")  # debug
 
 server_socket.bind((IP, PORT))
-print ("socket binded to %s" %(PORT)) # debug
+print("Socket bound to %s" %(PORT))  # debug
 print("Waiting for client response...")  # debug
 
 # receiving data from client
@@ -18,6 +18,27 @@ while True:
     client_id = message[6:-1]  # extract the client ID
 
     response_message = f'Hello, {client_id}! Welcome to the server!'
-    server_socket.sendto(response_message.encode(), client_address) # Response message
+    server_socket.sendto(response_message.encode(), client_address)  # Response message
+    print("Connection established with client")  # debug
 
-    break # connection closed
+    break  # connection closed
+
+# TCP Socket
+TCP_IP = "127.0.0.1"
+TCP_PORT = 5678
+
+tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # #AF_INET: internet, SOCK_STREAM: TCP
+tcp_server.bind((TCP_IP, TCP_PORT))
+tcp_server.listen()
+
+
+while True:
+    client, address = tcp_server.accept()  # Returns client socket and address
+    print(f"Connected to {address}")
+    print(client.recv(1024).decode('utf-8'))  # buffer size not that important, if it goes over it will be saved
+                                              # make sure to encode with utf -8 as well
+    client.send("Hello Client!".encode('utf-8'))
+    print(client.recv(1024).decode('utf-8'))
+    client.send("Bye!".encode('utf-8'))
+    client.close()
+    break  # connection closed
