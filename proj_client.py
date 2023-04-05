4abc52339caea18ff47d20cd9e5246a5fdcfabf2
zia01 <90281305+naklak@users.noreply.github.com>
2023-04-04 20:17:19 -0500

    merge, setup and debug
    Greg's branch merged. defined some functions for easy debug. Verification complete.

--- proj_client.py
+++ proj_client.py
@@ -1,31 +1,55 @@
 import socket
 
-IP = "127.0.0.1"
-PORT = 1234
-client_id = 'mockclient'  # hardcoded client ID: has to be in subscribed users list
+def udpsend(udp_socket, server_address, message):
+    udp_socket.sendto(message.encode(), server_address)
+    print("You: " + message)
 
-# create socket
-server_address = ("127.0.0.1", 1234)
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # AF_INET = internet, SOCK_DGRAM = UDP
-print("Socket successfully created")  # debug
+def udpreceive(udp_socket):
+    response, server_address = udp_socket.recvfrom(1024) # server response
+    print("Server: " + response.decode())
+    return response, server_address
+
+def tcpsend(tcp_socket, message):
+    tcp_socket.send(message.encode('utf-8'))
+    print("You: " + message)
+
+def tcpreceive(tcp_socket):
+    message, server_address = tcp_socket.recvfrom(1024)
+    print('Server: ', message.decode('utf-8'))
+    return message, server_address
+
+#####################################################
+
+# predefine values
+client_id = 'clientA'  # hardcoded client ID: has to be in subscribed users list
+secret_key = 100
+
+IP = "127.0.0.1"
+UDP_PORT = 1234
+
+# UDP socket creation
+server_address = (IP, UDP_PORT)
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # AF_INET = internet, SOCK_DGRAM = UDP
+print("\n* UDP socket created\n")
 
 # HELLO
-hello_message = f'HELLO({client_id})'  # protocol message: syntax may be changed
-client_socket.sendto(hello_message.encode(), server_address)
-response, server_address = client_socket.recvfrom(1024) # Wait for the server to respond
-print("Server says: " + response.decode())  # Debug
+udpsend(udp_socket, server_address, f'HELLO({client_id})')
+response, server_address = udpreceive(udp_socket)
 
-# close the socket
-client_socket.close()
+# TODO: authentication
+#   - receive CHALLENGE, respond with RESPONSE
+#   - Generate CK-A key, receive and decrypt the AUTH-SUCCESS message
 
-#create tcp socket
-TCP_IP = '127.0.0.1'
+# TCP Socket
 TCP_PORT = 5678
-tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_client.connect((TCP_IP, TCP_PORT))
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET: internet, SOCK_STREAM: TCP
+tcp_socket.connect((IP, TCP_PORT))
+print("\n* TCP socket created\n")  # debug
 
-tcp_client.send("Hello Server!".encode('utf-8'))
-print(tcp_client.recv(1024).decode('utf-8'))
-tcp_client.send("Bye!".encode('utf-8'))
-print(tcp_client.recv(1024).decode('utf-8'))
-tcp_client.close()
+tcpsend(tcp_socket, "Hello Server!")
+tcpreceive(tcp_socket)
+
+# TODO: connection
+    #   - send CONNECT, receive CONNECTED
+
+udp_socket.close()
--- proj_server.py
+++ proj_server.py
@@ -1,44 +1,70 @@
 import socket
 
-IP = "127.0.0.1"
-PORT = 1234
+def udpsend(udp_socket, client_address, message):
+    udp_socket.sendto(message.encode(), client_address)
+    print("You: " + message) # debug
 
-# Create socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # AF_INET: internet, SOCK_DGRAM: UDP
-print("Socket successfully created")  # debug
-
-server_socket.bind((IP, PORT))
-print("Socket bound to %s" %(PORT))  # debug
-print("Waiting for client response...")  # debug
-
-# receiving data from client
-while True:
-    message, client_address = server_socket.recvfrom(1024) # receive HELLO message
+def udpreceive(udp_socket):
+    message, client_address = udp_socket.recvfrom(1024) # receive HELLO message
     message = message.decode()
-    client_id = message[6:-1]  # extract the client ID
+    print("Client: " + message)
+    return message, client_address
 
-    response_message = f'Hello, {client_id}! Welcome to the server!'
-    server_socket.sendto(response_message.encode(), client_address)  # Response message
-    print("Connection established with client")  # debug
+def tcpsend(client_socket, message):
+    print("You: " + message)
+    client_socket.send(message.encode('utf-8')) # TODO: the encoding will be replaced with CK-A
 
-    break  # connection closed
+def tcpreceive(client_socket):
+    message = client_socket.recv(1024).decode()
+    print("Client: " + message) # buffer size not that important, if it goes over it will be saved
+    return message
 
-# TCP Socket
-TCP_IP = "127.0.0.1"
-TCP_PORT = 5678
+#####################################################
 
-tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # #AF_INET: internet, SOCK_STREAM: TCP
-tcp_server.bind((TCP_IP, TCP_PORT))
-tcp_server.listen()
+# predefined values
+subscriber_list = [('clientA', 100),('clientB', 200),('clientC', 300)] # predefined subscriber list
+subscriber_search = dict(subscriber_list)
 
+IP = '127.0.0.1'
+UDP_PORT = 1234
 
-while True:
-    client, address = tcp_server.accept()  # Returns client socket and address
-    print(f"Connected to {address}")
-    print(client.recv(1024).decode('utf-8'))  # buffer size not that important, if it goes over it will be saved
-                                              # make sure to encode with utf -8 as well
-    client.send("Hello Client!".encode('utf-8'))
-    print(client.recv(1024).decode('utf-8'))
-    client.send("Bye!".encode('utf-8'))
-    client.close()
-    break  # connection closed
+# UDP socket creation
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # AF_INET: internet, SOCK_DGRAM: UDP
+udp_socket.bind((IP, UDP_PORT))
+print("\n* UDP socket bound to %s" %(UDP_PORT))  # debug
+print("* Waiting for client response...\n")  # debug
+
+# HELLO
+message, client_address = udpreceive(udp_socket)
+client_id = message[6:-1]  # extract the client ID
+
+# Verification
+if client_id in subscriber_search:
+    udpsend(udp_socket, client_address, f"Hello, {client_id}! Welcome to the server!")
+
+    # TODO: authentication
+    #   - retrieve the client's secret key
+    #   - send CHALLENGE message
+    #   - receive RESPONSE
+    #   - if failure: send AUTH-FAIL message
+    #   - if success: generate encryption key CK-A and send AUTH-SUCCESS encrypted in that key
+
+    # TCP Socket
+    TCP_PORT = 5678
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # #AF_INET: internet, SOCK_STREAM: TCP
+    tcp_socket.bind((IP, TCP_PORT))
+    tcp_socket.listen()
+
+    client_socket, address = tcp_socket.accept() # Returns client socket and address
+    print("\n* TCP socket bound to %s" %(TCP_PORT))
+
+    tcpreceive(client_socket)
+    tcpsend(client_socket, "Hello Client!")
+
+    # TODO: connection
+        #   - receive CONNECT, send CONNECTED
+
+else:
+    print("Client is not in the subscriber list.")
+
+udp_socket.close()
