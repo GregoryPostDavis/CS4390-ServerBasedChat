b75febe2431003a2e536abb3e944598fdfdce927
jxp163630 <72633166+jxp163630@users.noreply.github.com>
2023-04-23 02:23:36 -0500

    Log On Feature
    "Log On" feature is added to the client.py

--- client.py
+++ client.py
@@ -34,45 +34,48 @@ server_address = (IP, UDP_PORT)
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print("\n* UDP socket created\n")
 
-# Log on
-udpsend(udp_socket, server_address, f"HELLO({client_id})") # Protocol: send HELLO(client_ID)
+val = input("Enter Your Command: ")
+while(val.strip().lower() == "log on"):                        # LOG ON
 
-# Authentication
-response, server_address = udpreceive(udp_socket) # Protocol: receive CHALLENGE(rand)
-challenge_message = response.decode()
-challenge_message = challenge_message[10:-1] # Extract rand
+    udpsend(udp_socket, server_address, f"HELLO({client_id})") # Protocol: send HELLO(client_ID)
 
-res = authentication.client_hash(challenge_message, secret_key)
-udpsend(udp_socket, server_address, f"RESPONSE({client_id}, {res})") # Protocol: send RESPONSE(res)
+    # Authentication
+    response, server_address = udpreceive(udp_socket) # Protocol: receive CHALLENGE(rand)
+    challenge_message = response.decode()
+    challenge_message = challenge_message[10:-1] # Extract rand
 
-ck_a = encryption.cipher_key(challenge_message, secret_key)
-#print("\nCK_A: %s" %(ck_a))                                    # - DEBUG
+    res = authentication.client_hash(challenge_message, secret_key)
+    udpsend(udp_socket, server_address, f"RESPONSE({client_id}, {res})") # Protocol: send RESPONSE(res)
 
-response, server_address = udp_socket.recvfrom(1024)
-response = encryption.decrypt_msg(response.decode(), ck_a)
-print("Server: " + response) # Protocol: receive AUTH_SUCCESS(rand_cookie, port_number)
+    ck_a = encryption.cipher_key(challenge_message, secret_key)
+    #print("\nCK_A: %s" %(ck_a))                                    # - DEBUG
 
-RAND_COOKIE = response[13:-7] # Extract cookie
-TCP_PORT = int(response[-5:-1]) # Extract tcp port number
+    response, server_address = udp_socket.recvfrom(1024)
+    response = encryption.decrypt_msg(response.decode(), ck_a)
+    print("Server: " + response) # Protocol: receive AUTH_SUCCESS(rand_cookie, port_number)
 
-#print("\nRand_Cookie: ", RAND_COOKIE)                           # - DEBUG
-#print("TCP Port Numer: ", TCP_PORT)                         # - DEBUG
- 
-# TCP Socket establishment
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_socket.connect((IP, TCP_PORT))
-print("\n* TCP socket created\n")                               # - DEBUG
+    RAND_COOKIE = response[13:-7] # Extract cookie
+    TCP_PORT = int(response[-5:-1]) # Extract tcp port number
 
-tcpsend(tcp_socket, f"CONNECT({RAND_COOKIE})", ck_a) # Protocol: send CONNECT(rand_cookie)
-tcpreceive(tcp_socket) # Protocol: receive CONNECTED
+    #print("\nRand_Cookie: ", RAND_COOKIE)                           # - DEBUG
+    #print("TCP Port Numer: ", TCP_PORT)                         # - DEBUG
+    
+    # TCP Socket establishment
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((IP, TCP_PORT))
+    print("\n* TCP socket created\n")                               # - DEBUG
 
-# Send messages
-while True:
-    msg = input("You: ")
-    tcpsend(tcp_socket, msg, ck_a) 
-    if msg.strip().lower() == "log off":
-        print("Logging off...\n")
-        break
+    tcpsend(tcp_socket, f"CONNECT({RAND_COOKIE})", ck_a) # Protocol: send CONNECT(rand_cookie)
+    tcpreceive(tcp_socket) # Protocol: receive CONNECTED
+
+    # Send messages
+    while True:
+        msg = input("You: ")
+        tcpsend(tcp_socket, msg, ck_a) 
+        if msg.strip().lower() == "log off":
+            print("Logging off...\n")
+            break
+    break                                   # BREAK OUT OF "LOG ON" WHILE LOOP
 
 tcp_socket.close()
 print("* TCP connection closed.")
